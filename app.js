4c07d97b7130e0a3e4c968ab4e68bf88e49e9d5b
Andrew Harasymiw <aharasymiw@gmail.com>
2016-01-12 13:57:14 -0600

    finished custome directives example

--- app.js
+++ app.js
@@ -1,36 +1,72 @@
 (function() {
   var app = angular.module('gemStore', []);
 
+  app.directive('productGallery', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(imageNumber) {
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
   app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller('TabController', function() {
-    this.tab = 1;
-
-    this.setTab = function(newValue) {
-      this.tab = newValue;
-    };
-
-    this.isSet = function(tabName) {
-      return this.tab === tabName;
-    };
-  });
-
-  app.controller('GalleryController', function() {
-    this.current = 0;
-    this.setCurrent = function(newGallery) {
-      this.current = newGallery || 0;
-    };
-  });
-
   app.controller('ReviewController', function() {
+
     this.review = {};
 
     this.addReview = function(product) {
-      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    };
+
+  });
+
+  app.directive('productDescriptions', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-description.html'
+    };
+  });
+
+  app.directive('productReviews', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-reviews.html'
+    };
+  });
+
+  app.directive('productSpecs', function() {
+    return {
+      restrict: 'A',
+      templateUrl: 'product-specs.html'
+    };
+  });
+
+  app.directive('productTabs', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controller: function() {
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(activeTab) {
+          this.tab = activeTab;
+        };
+      },
+      controllerAs: 'tab'
     };
   });
 
